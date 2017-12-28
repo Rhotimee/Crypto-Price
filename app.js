a0231f1b8fa23a6b21dc8030f5d1140f75cff9e4
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2017-12-28 10:07:55 +0100

    initial setup

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
+import { error } from 'util';
+
 const yargs = require('yargs');
+const request = require('request');
 
 const argv = yargs
     
@@ -8,3 +11,13 @@ const argv = yargs
 
 command = argv._[0].toUpperCase()    
 console.log(command)
+
+request({
+    url: `https://api.coinmarketcap.com/v1/ticker/`,
+    json: true
+}, (error, response, body) => {
+    console.log(error);
+    console.log(response && response.statusCode)
+    console.log(body)
+}
+);
