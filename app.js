a6334e9807ddfa6e859a8825d119a5a9535b7324
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2017-12-28 10:46:26 +0100

    print out name, price and market cap of cryptos

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-import { error } from 'util';
-
 const yargs = require('yargs');
 const request = require('request');
 
@@ -9,15 +7,20 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-command = argv._[0].toUpperCase()    
-console.log(command)
+command = argv._[0].toUpperCase()
+command2 = argv._[0].toLowerCase()
 
 request({
     url: `https://api.coinmarketcap.com/v1/ticker/`,
     json: true
 }, (error, response, body) => {
-    console.log(error);
-    console.log(response && response.statusCode)
-    console.log(body)
+    // console.log(body[0].name);
+    for (let i = 0; i < body.length; i++){
+        if (body[i].symbol === command || body[i].id === command2){
+            console.log(`Name: ${body[i].name}`)
+            console.log(`Current Price: $${body[i].price_usd} USD`)
+            console.log(`Market Cap: $${body[i].market_cap_usd} USD`)
+        }
+    }
 }
 );
