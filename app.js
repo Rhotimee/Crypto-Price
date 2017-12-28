995528e7480384b96998e17e51e7e22fcd873564
Yemitan Isaiah Rotimi <isaroy007@gmail.com>
2017-12-28 14:15:05 +0100

    refactoring

--- app.js
+++ app.js
@@ -7,20 +7,21 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-command = argv._[0].toUpperCase()
-command2 = argv._[0].toLowerCase()
+let command = argv._[0].toUpperCase();
+let command2 = argv._[0].toLowerCase();
 
 request({
     url: `https://api.coinmarketcap.com/v1/ticker/`,
     json: true
 }, (error, response, body) => {
-    // console.log(body[0].name);
-    for (let i = 0; i < body.length; i++){
-        if (body[i].symbol === command || body[i].id === command2){
-            console.log(`Name: ${body[i].name}`)
-            console.log(`Current Price: $${body[i].price_usd} USD`)
-            console.log(`Market Cap: $${body[i].market_cap_usd} USD`)
-        }
-    }
-}
-);
+
+    if (!error && response.statusCode === 200){
+        for (let i = 0; i < body.length; i++){
+            if (body[i].symbol === command || body[i].id === command2){
+                console.log(`Name: ${body[i].name}`);
+                console.log(`Current Price: $${body[i].price_usd} USD`);
+                console.log(`Market Cap: $${body[i].market_cap_usd} USD`);
+            } 
+        } 
+    } else {console.log(`The server could not be reached`)}   
+});
